ab5b93eb5fc6e9263a2bd58d1c463145e2bb361b
VGmt <Victor.guillemot@gmail.com>
2024-04-17 15:54:11 -0400

    design part 3

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,24 +1,26 @@
 import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import Header from '../partials/Header';
+import backgroundImage2 from '../images/IMG_8105.jpeg';
+import backgroundImage1 from '../images/Desktop-Stripe-integration-2.png';
 
 function SignUp() {
 
   // Initialize Stripe.js
   const stripe = Stripe('pk_live_51OaSWnEQO3gNE6xrKK1pHZXzWux71xpxXpA3nQNtNK30Vz43sCQeJzO7QuMk708tOGvGstsLbBS1jtMCIWZ14UCR00j1Bt80cF');
-  
+
   useEffect(() => {
     // Only initialize Stripe and Checkout once when the component mounts
     initialize();
   }, []);
-  
+
   // Fetch Checkout Session and retrieve the client secret
   async function initialize() {
     const fetchClientSecret = async () => {
       const response = await fetch("https://us-central1-trk-app-505a1.cloudfunctions.net/createCheckoutSession", {
         method: "POST",
         headers: {
-            'Content-Type': 'application/json',
+          'Content-Type': 'application/json',
         },
         body: JSON.stringify({
           // Example amount and currency - adjust according to your needs
@@ -43,32 +45,14 @@ function SignUp() {
 
   return (
     <div className="flex flex-col min-h-screen overflow-hidden">
-
-      {/*  Site header */}
-      <Header />
-
       {/*  Page content */}
-      <main className="flex-grow">
-
-        <section className="bg-gradient-to-b from-gray-100 to-white">
-          <div className="max-w-6xl mx-auto px-4 sm:px-6">
-            <div className="pt-32 pb-12 md:pt-40 md:pb-20">
-
-              {/* Page header */}
-              <div className="max-w-3xl mx-auto text-center pb-12 md:pb-20">
-                <h1 className="h1">Welcome. We exist to make entrepreneurism easier.</h1>
-              </div>
-
-              {/* Form */}
-              <div className="max-w-lg mx-auto">
-              <div id="checkout">
-              </div>
-              </div>
-
-            </div>
-          </div>
-        </section>
-
+      <main className="flex-grow flex md:flex-row flex-col">
+        <div className="w-full md:w-1/2">
+          <img src={backgroundImage2} alt="Background" className="w-full h-auto md:h-full object-cover" />
+        </div>
+        <div id="checkout" className="w-full md:w-1/2 p-4 md:p-8 bg-white">
+          {/* Stripe Checkout will be mounted here */}
+        </div>
       </main>
     </div>
   );
